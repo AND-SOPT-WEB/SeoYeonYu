21d7c70d50d6495b6efaa0c4ecaa6400e4d25e32
youtheyeon <yuseoyeon@ewhain.net>
2024-11-04 14:04:12 +0900

    style: TopBar 퍼블리싱

--- week3/assignment/src/App.jsx
+++ week3/assignment/src/App.jsx
@@ -1,5 +1,16 @@
+import { useState } from "react";
+
+import TopBar from "./components/TopBar";
+
 function App() {
-  return <></>;
+  const [selectedButton, setSelectedButton] = useState("게임");
+  const [level, setLevel] = useState(1);
+
+  return (
+    <>
+      <TopBar {...{ selectedButton, setSelectedButton, level, setLevel }} />
+    </>
+  );
 }
 
 export default App;
---
+++ week3/assignment/src/components/TopBar.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+
+const TopBar = ({ selectedButton, setSelectedButton, level, setLevel }) => {
+  const buttons = ["게임", "랭킹"];
+  const levels = [1, 2, 3];
+
+  const handleLevelChange = (e) => {
+    setLevel(e.target.value);
+  };
+
+  return (
+    <Container>
+      <h1>1 to 50</h1>
+      <SectionContainer>
+        <Section>
+          {buttons.map((text) => (
+            <Button
+              key={text}
+              isSelected={selectedButton === text}
+              onClick={() => setSelectedButton(text)}
+            >
+              {text}
+            </Button>
+          ))}
+        </Section>
+
+        {selectedButton === "게임" && (
+          <Section>
+            <LevelSelect value={level} onChange={handleLevelChange}>
+              {levels.map((num) => (
+                <option key={num} value={num}>
+                  Level {num}
+                </option>
+              ))}
+            </LevelSelect>
+          </Section>
+        )}
+      </SectionContainer>
+    </Container>
+  );
+};
+
+export default TopBar;
+
+const Container = styled.header`
+  padding: 0 4rem;
+  display: flex;
+  align-items: center;
+  gap: 4rem;
+  height: 5rem;
+  background-color: var(--red);
+
+  h1 {
+    font-size: 2rem;
+    font-weight: 800;
+    flex-shrink: 0;
+  }
+`;
+
+const SectionContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const Section = styled.section`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 0.6rem 1.2rem;
+  background-color: ${({ isSelected }) => (isSelected ? "var(--orange)" : "transparent")};
+  color: white;
+  font-weight: 600;
+  border-radius: 8px;
+  cursor: pointer;
+`;
+
+const LevelSelect = styled.select`
+  padding: 0.5rem;
+  border-radius: 8px;
+`;
