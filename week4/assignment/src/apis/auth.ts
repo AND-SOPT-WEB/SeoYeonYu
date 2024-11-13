57dcc909411bd4db9e5297d1c6ce34bf4cf50dd9
youtheyeon <yuseoyeon@ewhain.net>
2024-11-13 16:38:12 +0900

    fix: axios error 해결

--- week4/assignment/src/apis/auth.ts
+++ week4/assignment/src/apis/auth.ts
@@ -1,4 +1,5 @@
 import instance from "./http";
+import axios from "axios";
 
 // POST : 로그인
 export const PostLogin = async (name: string, password: string) => {
@@ -7,13 +8,16 @@ export const PostLogin = async (name: string, password: string) => {
       username: name,
       password: password,
     });
-    localStorage.setItem("token", response.data.result.token);
     return response;
   } catch (error) {
-    if (error.status === 400) {
-      alert("아이디 또는 비밀번호를 정확히 입력해주세요.");
-    } else if (error.status === 403) {
-      alert("잘못된 비밀번호입니다.");
+    if (axios.isAxiosError(error)) {
+      if (error.response?.status === 400) {
+        alert("아이디 또는 비밀번호를 정확히 입력해주세요.");
+      } else if (error.response?.status === 403) {
+        alert("비밀번호가 올바르지 않습니다.");
+      }
+    } else {
+      alert("로그인에 실패했습니다.");
     }
     throw error;
   }
@@ -30,7 +34,7 @@ export const PostSignUp = async (name: string, password: string, hobby: string) 
     alert(`회원가입 성공! 회원 번호: ${response.data.result.no}`);
     return response;
   } catch (error) {
-    if (error.status === 409) {
+    if (axios.isAxiosError(error) && error.status === 409) {
       alert("이미 존재하는 사용자 이름입니다.");
     } else {
       alert("회원가입에 실패했습니다.");
@@ -55,7 +59,7 @@ export const GetUserHobby = async (userNo: string) => {
     const response = await instance.get(`/user/${userNo}/hobby`);
     return response.data.result.hobby;
   } catch (error) {
-    if (error.response?.data.code === "01") {
+    if (axios.isAxiosError(error) && error.response?.data.code === "01") {
       alert("해당 번호의 회원이 존재하지 않습니다.");
     } else {
       alert("취미 조회에 실패했습니다.");
@@ -74,9 +78,14 @@ export const PutUserInfo = async (hobby: string, password: string) => {
     alert("정보가 성공적으로 변경되었습니다.");
     return response;
   } catch (error) {
-    if (error.response?.status === 400 && error.response?.data?.code === "00") {
-      alert("취미 또는 비밀번호를 8자 이하로 입력해주세요.");
+    if (axios.isAxiosError(error)) {
+      if (error.response?.status === 400 && error.response?.data?.code === "00") {
+        alert("취미 또는 비밀번호를 8자 이하로 입력해주세요.");
+      }
+    } else {
+      alert("정보 변경에 실패했습니다.");
     }
+
     throw error;
   }
 };
